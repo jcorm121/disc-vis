9017491c258eb91778923f751149cb87666913c9
Cormac Kennedy <cjk276@cornell.edu>
2026-06-17 16:40:55 -0400

    Initial Commit

---
+++ disc-vis/disc_visApp.swift
@@ -0,0 +1,17 @@
+//
+//  disc_visApp.swift
+//  disc-vis
+//
+//  Created by Cormac Kennedy on 6/17/26.
+//
+
+import SwiftUI
+
+@main
+struct disc_visApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
